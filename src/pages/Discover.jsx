438ef244fb49e29f54e1ec7bb6bfbf764cb1be5c
Vikash-Pareek <vikash.pareek1509@gmail.com>
2022-11-20 20:39:56 +0530

    discover page initial setup

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { Error, Loader, SongCard } from "../components";
+import { songGenres } from "../assets/constants";
 
 const Discover = () => {
+  const songGenreTitle = "Pop";
+
   return (
-    <div>
-      <h1 className="text-3xl underline">Discover</h1>
+    <div className="flex flex-col">
+      <div className="flex w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="text-left text-3xl font-bold">
+          Discover {songGenreTitle}
+        </h2>
+        <select className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
+          {songGenres.map((item) => {
+            return (
+              <option key={item.value} value={item.value}>
+                {item.title}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      <div></div>
     </div>
   );
 };
